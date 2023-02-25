c6663fa08dd299c88de30f95f2c0922a7e6866dd
Qual09 <cpx.hieu@gmail.com>
2023-02-25 22:22:46 +0100

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,47 +1,47 @@
-// Imports
+// ### Imports
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
-// Local Modules
+// ### Local Modules
 const userRoutes = require('./api/user');
 
-// Environment Viariables
+// ### Environment Viariables
 dotenv.config();
 const apiUrl = '/api';
-const clientApp = '/../public';
+const clientApp = '../public';
 const viewsFolder = 'views';
 
-// App
+// ### App
 const app = express();
 
-// Middlewares
+// ### Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
 
-// Views
-app.use('/', express.static(__dirname + clientApp));
+// ### Views
+app.use('/', express.static(path.join(__dirname, clientApp)));
 app.use('/server', express.static(path.join(__dirname, viewsFolder)));
 
-// REST API
+// ### REST API
 app.get(`${apiUrl}/`, (req, res) => {
   res.json({ info: 'Nodejs REST API' });
 });
 app.use(`${apiUrl}/user`, userRoutes);
 
-// Start Server
+// ### Start Server
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(process.env.TEST || '');
   console.log(`Server started on http://${HOSTNAME}:${PORT}`);
 });
 
-// Stop Server
+// ### Stop Server
 process.on('SIGTERM', () => {
   server.close(() => {
     console.log('Server closed: Process Terminated!');
