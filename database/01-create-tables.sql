d0352dd08561a0aa57a301c0624573975fb8859b
Qual09 <cpx.hieu@gmail.com>
2023-02-27 01:04:46 +0100

    fix

--- database/01-create-tables.sql
+++ database/01-create-tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
   last_name text,
   email text,
   photo_url text,
-  access text DEFAULT "user",
+  access text DEFAULT 'user',
   create_date timestamp DEFAULT now(),
   create_user text,
   update_date timestamp DEFAULT now(),
