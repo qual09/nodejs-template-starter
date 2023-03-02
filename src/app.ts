769d7d4be3ae727e7937f1b7c7011031030d4afb
Qual09 <cpx.hieu@gmail.com>
2023-03-03 00:27:30 +0100

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-// Imports
+// ### Imports
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
@@ -7,30 +7,30 @@ import dotenv from 'dotenv';
 import { Server } from 'http';
 import { Application, Request, Response, NextFunction } from 'express';
 
-// Local Modules
+// ### Local Modules
 import { userRoutes } from './api/user';
 import { authRoutes } from './api/auth';
 
-// Environment Viariables
+// ### Environment Viariables
 dotenv.config();
 const apiUrl: string = '/api';
 const clientApp: string = '/../public';
 const viewsFolder: string = 'views';
 
-// App
+// ### App
 const app: Application = express();
 
-// Middlewares
+// ### Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
 
-// Views
+// ### Views
 app.use('/', express.static(__dirname + clientApp));
 app.use('/server', express.static(path.join(__dirname, viewsFolder)));
 
-// REST API
+// ### REST API
 app.get(`${apiUrl}/`, (req: Request, res: Response, next: NextFunction) => {
   const currentDate: string = new Date().toISOString();
   res.json({ info: 'Nodejs REST API', timestamp: currentDate });
@@ -38,7 +38,7 @@ app.get(`${apiUrl}/`, (req: Request, res: Response, next: NextFunction) => {
 app.use(`${apiUrl}/user`, userRoutes);
 app.use(`${apiUrl}/auth`, authRoutes);
 
-// Start Server
+// ### Start Server
 const HOSTNAME: string = process.env.HOSTNAME || 'localhost';
 const PORT: number = Number(process.env.PORT) || 3000;
 const server: Server = app.listen(PORT, () => {
@@ -46,7 +46,7 @@ const server: Server = app.listen(PORT, () => {
   console.log(`Server started on http://${HOSTNAME}:${PORT}`);
 });
 
-// Stop Server
+// ### Stop Server
 process.on('SIGTERM', () => {
   server.close(() => {
     console.log('Server closed: Process Terminated!');
